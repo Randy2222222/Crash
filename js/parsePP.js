f538e8cf9e19b5633f12a7731212c942504be735
Randy2222222 <randyjpollitt@yahoo.com>
2026-01-03 13:33:08 -0500

    Update regex for comment matching in parsePP.js

--- js/parsePP.js
+++ js/parsePP.js
@@ -551,7 +551,7 @@ if (currentPPspd === null && SPD_REGEX.test(trimmed)) {
         //  continue;
       }
       // 💬 Comments about Race 💬
-     const commentM = trimmed.match(/^[A-Za-z0-9 ;,.'’\-\/\s]+$/);
+     const commentM = trimmed.match(/^[A-Za-z  ]+((\d\_\-\’\!\,\;\:\+\ ?)(\d\/\d?)?)/);
             if (commentM) {
                currentPPcomment = commentM[0];
           //   continue;
