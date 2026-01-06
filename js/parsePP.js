08b5d98b8d92d300a174ba6373872339333fd692
Randy2222222 <randyjpollitt@yahoo.com>
2026-01-05 21:24:53 -0500

    Fix regex replacement for whitespace in comments

--- js/parsePP.js
+++ js/parsePP.js
@@ -542,7 +542,7 @@ if (currentPPspd === null && SPD_REGEX.test(trimmed)) {
       
         const commentM = trimmed.match(/([a-z]+\s[a-z]+[a-z]+|[A-Z][a-z]+|\d[A-Za-z]|,'_|\;|\-)/g);
             if (commentM) {
-              currentPPcomment = commentM.join(" ").replace(/\/g, " ").trim();
+              currentPPcomment = commentM.join(" ").replace(/\s/g, " ").trim();
              continue;
          } 
         // 🏁 How Many 🏇 Horses Raced 🏁
